3f9b9b5302b542b96e42174a9e4e39844a8e7b0a
arsk-39 <ryoji.arisaka@acreation.co.jp>
2023-05-31 14:42:00 +0900

    a

--- Java リソース/jp/co/aforce/beans/LoginServlet.java
+++ Java リソース/jp/co/aforce/beans/LoginServlet.java
@@ -15,7 +15,7 @@ import been.login;
 /**
  * Servlet implementation class Login
  */
-@WebServlet("/jsp/login")
+@WebServlet("/LoginSampleV1/jsp/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
